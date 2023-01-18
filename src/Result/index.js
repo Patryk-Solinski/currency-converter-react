1cf10333002f7c1cf96e499353e9dacd71f35684
Patryk Solinski <solinskipatryk8@gmail.com>
2023-01-18 17:57:46 +0100

    refactroring code

--- src/Result/index.js
+++ src/Result/index.js
@@ -1,13 +1,10 @@
+import "./style.css";
 
 const Result = ({ amount, rate, currency }) => {
     const result = amount / rate;
 
     return (
-        <p className="form__result">
-        <>
-            <strong>{result.toFixed(2)} {currency} </strong>
-        </>
-        </p>
+        <p className="form__result">{result.toFixed(2)} {currency}</p >
     );
 };
 
