9cd5ad4a5002f80b7b3247b02e6c1c473bbc94ac
Patryk Solinski <solinskipatryk8@gmail.com>
2023-01-18 17:37:36 +0100

    added className

--- src/Result/index.js
+++ src/Result/index.js
@@ -1,11 +1,14 @@
-const Result = ({amount, rate, currency}) => {
-    const result = amount/rate;
 
-return (
-    <>
-    {amount.toFixed(2)} PLN = <strong>{result.toFixed(2)} {currency} </strong>
-    </>
-);
+const Result = ({ amount, rate, currency }) => {
+    const result = amount / rate;
+
+    return (
+        <p className="form__result">
+        <>
+            <strong>{result.toFixed(2)} {currency} </strong>
+        </>
+        </p>
+    );
 };
 
 export default Result;
