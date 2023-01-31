583172e71058eec4c01954929776308f5b3aa677
Patryk Solinski <solinskipatryk8@gmail.com>
2023-01-31 16:14:37 +0100

    import Clock

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import Form from "./Form";
 import Container from "./Container";
 import Footer from "./Footer";
 import Header from "./Header";
+import Clock from "./Clock";
 
 function App() {
   return (
     <Container>
+      <Clock />
       <Header />
       <Form />
       <Footer />
