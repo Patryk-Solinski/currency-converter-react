2e33e3012538150a51ffdc92f3ceb791d846c3de
Patryk Solinski <solinskipatryk8@gmail.com>
2023-01-31 20:56:15 +0100

    added formattedDate

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -1,10 +1,19 @@
 import { useEffect, useState } from "react"
 import "./style.css";
-import { formattedDate } from "./fromattedDate";
+
 
 const Clock = () => {
+    const formattedDate = 
+    {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+    };
+   
     const [date, setDate] = useState(new Date());
-
 
     useEffect(() => {
         const intervalId = setInterval(() => {
