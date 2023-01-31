437c81122bd623be24af31748b7d1c381557fb75
Patryk Solinski <solinskipatryk8@gmail.com>
2023-01-31 16:45:49 +0100

    formatted date

---
+++ src/Clock/fromattedDate.js
@@ -0,0 +1,11 @@
+export const formattedDate = 
+    {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+    };
+   
+
