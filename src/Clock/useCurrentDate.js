d221e5fa8c42e11382c0f624d4b6162c7d2d06d6
Patryk Solinski <solinskipatryk8@gmail.com>
2023-02-07 13:14:36 +0100

    removed formatDate

--- src/Clock/useCurrentDate.js
+++ src/Clock/useCurrentDate.js
@@ -1,14 +1,6 @@
 import { useState, useEffect } from "react";
 
-const formatDate =
-{
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-};
+
 
 export const useCurrentDate = () => {
     const [date, setDate] = useState(new Date());
@@ -23,7 +15,7 @@ export const useCurrentDate = () => {
         };
     }, [date]);
 
-    const formattedDate = date.toLocaleString(undefined, formatDate);
 
-return formattedDate;
+
+return date;
 };
