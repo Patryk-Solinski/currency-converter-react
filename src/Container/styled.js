dcc428da82a5d706942a4dcacc03299437ae359d
Patryk Solinski <solinskipatryk8@gmail.com>
2023-02-07 13:16:56 +0100

    rename styled component

--- src/Container/styled.js
+++ src/Container/styled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.main`
+export const Container = styled.main`
     padding: 20px;
     background-color: hsl(240, 44%, 93%, 0.98);
     border-radius: 10px;
