0083c103a1d3c1803e8a4bf45bf6a6913e587ba3
Patryk Solinski <solinskipatryk8@gmail.com>
2023-02-07 13:14:08 +0100

    added function which change date

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -1,12 +1,25 @@
 import { StyledClock } from "./styled";
 import { useCurrentDate } from "./useCurrentDate";
 
+const formatDate = (date ) => date.toLocaleString(undefined,
+    {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+    });
+
+
 const Clock = () => {
     const date = useCurrentDate();
 
     return (
         <StyledClock>
-            Dzisiaj jest: {date}
+            Dzisiaj jest: 
+            {" "}
+            {formatDate(date)}
         </StyledClock>
     );
 };
