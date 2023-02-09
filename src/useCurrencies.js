4f8acde2a230f12357ebc6e4eeaf41c07c0cd708
Patryk Solinski <solinskipatryk8@gmail.com>
2023-02-09 23:23:59 +0100

    added useCurrencies

--- src/useCurrencies.js
+++ src/useCurrencies.js
@@ -1,21 +1,34 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 
-
-const dataApi = async () => {
-    try {
-        const response = await axios.get("https://api.exchangerate.host/latest?base=PLN");
-
-        setRates({
-            rates: response.data.rates,
-            status: "success",
-        });
-    } catch (error) {
-        setRates({
-            status: "error"
-        });
-    };
+export const useCurrencies = () => {
+    const [rates, setRates] = useState({
+        date: null,
+        rates: {},
+        status: "loading",
+    });
+    
+    useEffect(() => {
+        const dataApi = async () => {
+            try {
+                const response = await axios.get("https://api.exchangerate.host/latest?base=PLN");
+        
+                setRates({
+                    rates: response.data.rates,
+                    status: "success",
+                });
+            } catch (error) {
+                setRates({
+                    status: "error"
+                });
+            };
+        };
+        setTimeout(dataApi, 1000);
+    },[]);
+return rates;    
 };
-setTimeout(dataApi, 1000);
+
+
 
 
 
