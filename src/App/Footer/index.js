433d21bfd6e54c30da2fe1df5469a2458a257219
Patryk Solinski <solinskipatryk8@gmail.com>
2023-02-10 11:42:48 +0100

    added date from api

--- src/App/Footer/index.js
+++ src/App/Footer/index.js
@@ -1,12 +1,12 @@
 import { StyledFooter,Paragraph } from "./styled";
 
-const Footer = () => (
+const Footer = ({ratesData}) => (
     <StyledFooter>
         <Paragraph >
             EUR 4.7199 | USD 4.7477 | GBP 5.4733 | CHF 5.3451
         </Paragraph>
         <Paragraph>
-            Kursy walut według średniego kursu NBP z dn. 28.10.2022
+            Kursy walut według średniego kursu NBP z dn. {ratesData.date}
         </Paragraph>
     </StyledFooter>
 );
