e53adf2426cc974d52370eab4ae5ade585d0723f
Patryk Solinski <solinskipatryk8@gmail.com>
2023-02-10 11:44:04 +0100

    removed rates of currencies

--- src/App/Footer/index.js
+++ src/App/Footer/index.js
@@ -1,10 +1,7 @@
-import { StyledFooter,Paragraph } from "./styled";
+import { StyledFooter, Paragraph } from "./styled";
 
-const Footer = ({ratesData}) => (
+const Footer = ({ ratesData }) => (
     <StyledFooter>
-        <Paragraph >
-            EUR 4.7199 | USD 4.7477 | GBP 5.4733 | CHF 5.3451
-        </Paragraph>
         <Paragraph>
             Kursy walut według średniego kursu NBP z dn. {ratesData.date}
         </Paragraph>
