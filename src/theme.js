b6bae0551d0d6949d8e5c1c140d51ad57233b9f8
Patryk Solinski <solinskipatryk8@gmail.com>
2023-02-10 16:32:36 +0100

    added color and breakpoint

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+export const theme = {
+    color: {
+        white: "#fff",
+        silver: "#ccc",
+        black: "#000",
+        alto: "#ddd",
+        concord: "#7C7B7A",
+        orange: "#FF6600",
+        arapawa: "#191971",
+        blueHaze: "#e5e5f5"
+    },
+    breakpoint: {
+        phone: 767,
+    },
+};
